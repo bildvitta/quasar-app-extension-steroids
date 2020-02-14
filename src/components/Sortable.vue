32de73d98b4fadc7de6b5fdf965e4c4f7a69cc8d
Douglas Calora <douglascalora123@gmail.com>
2020-02-14 15:24:21 -0300

    implementing sortable

--- src/components/Sortable.vue
+++ src/components/Sortable.vue
@@ -1,129 +1,118 @@
-<template>
-  <div ref="items" style="display: flex; justify-content: space-between; flex-wrap: wrap;">
-    <div v-for="(image, index) in value" :key="image.path" class="card">
-      <div>{{ image.name }}{{ image.order }}</div>
-      {{ image.path }} {{ image.oldOrder }}
-      {{ index }}
-    </div>
-  </div>
-</template>
+  <template>
+    <component :is="componenTag" ref="items" v-bind="$attrs">
+      <slot :order="order"/>
+    </component>
+  </template>
 
-<script>
-import Sortable from 'sortablejs'
-import { sortBy } from 'lodash'
+  <script>
+  import Sortable from 'sortablejs'
+  import { sortBy } from 'lodash'
 
-export default {
-  props: {
-    value: {
-      type: Array,
-      default: () => []
-    }
-  },
+  export default {
+    props: {
+      value: {
+        type: Array,
+        default: () => []
+      },
 
-  created () {
-    this.formatedValue = this.value
-    console.log(this.value)
-  },
-
-  data () {
-    return {
-      // images: [
-      //   {
-      //     path: 'test 0',
-      //     name: 'nome de teste',
-      //     order: 0
-      //   },
-      //   {
-      //     path: 'test 1',
-      //     name: 'nome de teste',
-      //     order: 1
-      //   },
-      //   {
-      //     path: 'test 2',
-      //     name: 'nome de teste',
-      //     order: 2
-      //   },
-      //   {
-      //     path: 'test 3',
-      //     name: 'nome de teste',
-      //     order: 3
-      //   },
-      //   {
-      //     path: 'test 4',
-      //     name: 'nome de teste',
-      //     order: 4
-      //   },
-      //   {
-      //     path: 'test 5',
-      //     name: 'nome de teste',
-      //     order: 5
-      //   },
-      // ]
-      formatedValue: null
-    }
-  },
-
-  computed: {
-    ids () {
-      return this.formatedValue.map(item => item.path)
-    }
-  },
-
-  mounted() {
-    new Sortable(this.$refs.items, {
-      animation: 150,
-
-      onUpdate: (event) => {
-        this.changeOrder(event)
+      componenTag: {
+        type: String,
+        default: 'div'
       }
-    })
-  },
+    },
 
-  methods: {
-    changeOrder ({ oldIndex, newIndex }) {
-      // this.$set(this.images[oldIndex], 'order', newIndex)
-      const cache = this.formatedValue[newIndex]
+    data () {
+      return {
+        formatedValue: null,
+        ids: null,
+        order: [0, 1, 2, 3, 4, 5]
+      }
+    },
 
-      this.$set(this.formatedValue, newIndex, this.formatedValue[oldIndex])
-      this.$set(this.formatedValue, oldIndex, cache)
-      // this.images[newIndex] = this.images[oldIndex]
-      // this.images[oldIndex] = cache
+    created () {
+      this.formatedValue = this.value
+      this.ids = this.formatedValue.map(item => item.path)
+
       this.$emit('input', this.ids)
+    },
 
-      console.log(this.ids)
+    mounted () {
+      new Sortable(this.$refs.items, {
+        animation: 300,
 
-      // if (oldIndex > newIndex) {
-      //   while (newIndex < oldIndex) {
-      //     this.$set(this.images[newIndex], 'order', this.images[newIndex].order + 1)
-      //     newIndex++
-      //   }
+        onStar: event => this.$emit('on-start', event),
 
-      //   sortBy(this.images, [image => image.order]).forEach((item, itemIndex) => {
-      //     this.$set(this.images, itemIndex, item)
-      //   })
+        onEnd: event => this.$emit('on-end', event),
 
-      // } else {
-      //   while (oldIndex < newIndex) {
-      //     this.$set(this.images[oldIndex + 1], 'order', this.images[oldIndex + 1].order - 1)
-      //     oldIndex++
-      //   }
+        onUpdate: event => {
+          this.changeOrder(event)
+          this.$emit('on-update', event)
+        }
+      })
+    },
 
-      //   sortBy(this.images, [image => image.order]).forEach((item, itemIndex) => {
-      //     this.$set(this.images, itemIndex, item)
-      //   })
-      // }
-    }
-  },
-}
-</script>
+    methods: {
+      changeOrder ({ oldIndex, newIndex }) {
+        // this.$set(this.images[oldIndex], 'order', newIndex)
+        // const cache = this.formatedValue[newIndex]
 
-<style lang="scss">
-  .card {
-    background-color: blue;
-    height: 300px;
-    margin: 20px;
-    color: white;
-    width: 25%;
-    font-size: 20px;
+        // this.$set(this.formatedValue, newIndex, this.formatedValue[oldIndex])
+        // this.$set(this.formatedValue, oldIndex, cache)
+        // this.order = []
+
+        const deleted = this.ids.splice(oldIndex, 1)
+        const cache = this.order[oldIndex]
+        this.ids.splice(newIndex, 0, deleted[0])
+        this.order.splice(oldIndex, 1)
+        this.order.splice(newIndex, 0, cache)
+        console.log(this.order, '<< epois')
+        // this.ids.forEach((id, index) => this.$set(this.order, this.order.length, { id, index}))
+        this.$emit('input', this.ids)
+
+        // this.ids.forEach((id, index) => {
+        //   console.log(id, index)
+        //   this.order.push({ id, index })
+        // })
+
+        console.log(this.ids)
+
+        try {
+          // const response = this.await
+        } catch (error) {
+          
+        }
+
+        // this.order = this.ids.map((id, index) => { id, index })
+        // this.ids.forEach((id, index) => this.order.push({ id, index}))
+
+        console.log(this.order)
+        // const newValue = this.formatedValue.filter((value, index) => value.path === this.ids[index])
+
+        // console.log(newValue)
+        // this.images[newIndex] = this.images[oldIndex]
+        // this.images[oldIndex] = cache
+
+        // if (oldIndex > newIndex) {
+        //   while (newIndex < oldIndex) {
+        //     this.$set(this.images[newIndex], 'order', this.images[newIndex].order + 1)
+        //     newIndex++
+        //   }
+
+        //   sortBy(this.images, [image => image.order]).forEach((item, itemIndex) => {
+        //     this.$set(this.images, itemIndex, item)
+        //   })
+
+        // } else {
+        //   while (oldIndex < newIndex) {
+        //     this.$set(this.images[oldIndex + 1], 'order', this.images[oldIndex + 1].order - 1)
+        //     oldIndex++
+        //   }
+
+        //   sortBy(this.images, [image => image.order]).forEach((item, itemIndex) => {
+        //     this.$set(this.images, itemIndex, item)
+        //   })
+        // }
+      }
+    },
   }
-</style>
+  </script>
