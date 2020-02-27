7bfc413e2e3fbe3c5ed4b3da499348a24db56c30
Douglas Calora <douglascalora123@gmail.com>
2020-02-27 17:39:27 -0300

    fixing bug in dialog router

--- src/components/DialogRouter.vue
+++ src/components/DialogRouter.vue
@@ -14,8 +14,13 @@ export default {
     return {
       component: null,
       parentRoute: '',
-      route: null
+      route: null,
+      isLoading: false
     }
+  },
+
+  created () {
+    console.log(history)
   },
 
   methods: {
@@ -36,19 +41,44 @@ export default {
     },
 
     resolveRoute (path) {
+      // console.log(this.$router.resolve(path).route, '<<<<')
       return this.$router.resolve(path).route
     },
 
-    show (route) {
+    async show (route) {
       this.parentRoute = this.$route.fullPath
       this.route = this.resolveRoute(route)
 
       if (history) {
-        history.replaceState(null, null, this.route.fullPath)
+        console.log(window.history, this.route.fullPath)
+        // console.log(history, this.route.fullPath)
+        // history.replaceState(null, null, this.route.fullPath)
+        history.replaceState({}, null, this.route.fullPath)
+        // this.$router.replace({ path: this.route.fullPath})
       }
 
+      // console.log(history)
+
       this.component = [...this.route.matched].pop().components.default
-      this.$refs.dialog.show()
+      this.$q.loading.show()
+      if (this.component) {
+        this.$refs.dialog.show()
+      } else {
+        }
+      this.$q.loading.hide()
+
+      // console.log(([...this.route.matched].pop().components.default)())
+      // try {
+      //   // await ([...this.route.matched].pop().components.default)() || []
+      //   this.component = [...this.route.matched].pop().components.default
+      //   console.log(this.route.matched)
+      //   this.$refs.dialog.show()
+      //   this.$q.loading.hide()
+      // } catch (error) {
+      //   console.log(error, 'error')
+      // } finally {
+      //   this.isLoading = false
+      // }
     }
   }
 }
