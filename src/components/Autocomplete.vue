97e0b8cebe0e4a3025e8406664c31ec532f96d03
Douglas Calora <douglascalora123@gmail.com>
2020-03-05 17:28:46 -0300

    implementing fuse

--- src/components/Autocomplete.vue
+++ src/components/Autocomplete.vue
@@ -1,175 +1,165 @@
 <template>
-  <div v-on="$listeners">
-    <div v-if="!select">
-      <q-input v-bind="inputProps" v-model="search" filled :debounce="debounce" :loading="loadingState" @input.native="setLoading($event.target.value)">
-        <template v-slot:append>
-          <q-btn v-if="hasResult" icon="close" flat @click="clearSeach"/>
-          <q-btn icon="search" flat/>
-        </template>
-      </q-input>
-      <slot :result="result">
-        <transition name="fade">
-          <div v-if="hasResult" class="shadow-1">
-            <q-list dense bordered padding class="rounded-borders">
-              <q-item v-for="(item, index) in result" :key="index" clickable v-ripple>
-                <q-item-section>
-                  <div @click="setSearch(item[mainKey])" v-html="formattedResult[index][mainKey]"/>
-                </q-item-section>
-              </q-item>
-          </q-list>
-          </div>
-        </transition>
-      </slot>
-    </div>
-    <q-select v-else v-bind="selectProps" :use-chips="multiple" :label="label" :options="filteredOptions" :multiple="multiple" outlined use-input input-debounce="0" v-on="$listeners" @filter="filterOptions">
-      <template v-slot:append>
-        <q-icon name="o_search" />
-      </template>
-      <template v-slot:no-option>
-        <q-item>
-          <q-item-section class="text-grey">
-            Nenhum resultado encontrado.
-          </q-item-section>
-        </q-item>
-      </template>
-    </q-select>
-  </div>
+  <q-select v-model="selectModel" v-bind="$attrs" v-on="$listeners" :options="filteredOptions" use-input map-options emit-value outlined input-debounce="0" @filter="filterOptions" @input="inputHandler" @input.native="clearSelectModel" @keyup.delete="clearSelectModel" clearable>
+    <template v-slot:append>
+      <q-icon name="o_search" />
+    </template>
+    <template v-slot:no-option>
+      <q-item>
+        <q-item-section class="text-grey">
+          Nenhum resultado encontrado.
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
 </template>
 
 <script>
+let fuse = null
+
 export default {
   props: {
-    select: {
-      type: Boolean
-    },
-
     list: {
       type: Array,
       default: () => []
     },
 
-    inputProps: {
+    labelKey: {
+      type: String,
+      default: ''
+    },
+
+    valueKey: {
+      type: String,
+      default: ''
+    },
+
+    options: {
       type: Object,
       default: () => ({})
-    },
-
-    selectProps: {
-      type: Object,
-      default: () => ({})
-    },
-
-    searchKeys: {
-      type: [String, Array],
-      default: 'title'
-    },
-
-    mainKey: {
-      type: String,
-      default: 'title'
-    },
-
-    debounce: {
-      type: String,
-      default: '500'
     }
   },
 
   data () {
     return {
       filteredOptions: [],
-      formattedResult: [],
       loadingState: false,
       result: [],
-      search: ''
+      selectModel: null,
+      search: '',
+      fuse: null,
+      fuseError: null,
+      fetchingFuse: false,
+      formatted: []
     }
   },
 
   watch: {
     search (value) {
       this.filter(value)
+    },
+
+    options () {
+      if (!this.fuseError || !this.fetchingFuse) {
+        fuse.options = { ...this.defaultOptions }
+      }
     }
+  },
+
+  mounted() {
+    console.log(this.$scopedSlots, this.$slots)
+  },
+
+  created () {
+    this.fetchFuse()
   },
 
   computed: {
     hasResult () {
       return this.result.length
+    },
+
+    formattedResult () {
+      if (!this.labelKey && !this.valueKey) {
+        return null
+      }
+
+      return this.list.map(item => this.format(item))
+    },
+
+    defaultOptions () {
+      return {
+        distance: 100,
+        keys: [ 'label', 'value'],
+        location: 0,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        shouldSort: true,
+        threshold: 0.1,
+        tokenize: true,
+        ...this.options
+      }
+    },
+
+    style () {
+      return { maxHeight: '65vh' }
     }
   },
 
   methods: {
-    filter (value) {
-      this.clearResults()
+    format (item) {
+      const fromTo = { label: this.labelKey, value: this.valueKey }
 
-      if (!value) {
-        return null
+      for (const key in fromTo) {
+        if (!item[key]) {
+          item[key] = item[fromTo[key]]
+
+          delete item[fromTo[key]]
+        }
       }
 
-      value = value.trim().replace(/\s{2,}/g, ' ').toLowerCase()
-
-      this.list.forEach(result => {
-        if (Array.isArray(this.searchKeys)) {
-          this.searchKeys.forEach(key => {
-            if (result[key] && result[key].toLowerCase().search(value) >= 0) {
-              this.increment(result, value)
-            }
-          })
-        } else if (result[this.searchKeys] && result[this.searchKeys].toLowerCase().search(value) >= 0) {
-          this.increment(result, value)
-        }
-      })
-
-      this.loadingState = false
+      return item
     },
 
-    increment (result, value) {
-      this.result.push(result)
-      this.formattedResult.push(result)
-      this.hightlight(result, value)
-    },
+    async fetchFuse () {
+      this.fetchingFuse = true
 
-    setLoading (value) {
-      if (!value) {
-        this.loadingState = false
-      } else {
-        this.loadingState = !this.hasResult
+      // import('fus.js').then().catch(() => console.log('tururu'))
+
+      try {
+        const response = (await (import('fuse.js'))).default
+
+        fuse = new response(this.list, this.defaultOptions)
+      } catch {
+        console.log('cai')
+        this.fuseError = true
+      } finally {
+        this.fetchingFuse = false
       }
-    },
-
-    hightlight (result, value) {
-      const index = this.formattedResult.length - 1
-
-      this.$set(
-        this.formattedResult,
-        index,
-        {
-          ...this.formattedResult[index],
-          [this.mainKey]: result[this.mainKey].toLowerCase().replace(value, `<span class="hightlight">${value}</span>`)
-        }
-      )
-    },
-
-    setSearch (value) {
-      this.search = value
-    },
-
-    clearSeach () {
-      this.search = ''
-    },
-
-    clearResults () {
-      this.result = []
-      this.formattedResult = []
     },
 
     filterOptions (value, update) {
       update(() => {
         if (value === '') {
-          this.filteredOptions = this.options
+          this.filteredOptions = this.formattedResult
         } else {
-          const filtered = value.toLowerCase()
-          this.filteredOptions = this.options.filter(item => item.label.toLowerCase().indexOf(filtered) > -1)
+          const filtered = value.toLowerCase().trim()
+
+          this.filteredOptions = (this.fuseError || this.fetchingFuse
+            ? this.formattedResult.filter(item => item.label.toLowerCase().indexOf(filtered) > -1)
+            : fuse.search(value)
+          )
         }
       })
+    },
+
+    inputHandler (value) {
+      this.$emit('input', this.selectModel)
+    },
+
+    clearSelectModel () {
+      if (this.selectModel && (!this.$attrs.multiple || !this.$attrs.useChips)) {
+        this.selectModel = ''
+      }
     }
   }
 }
