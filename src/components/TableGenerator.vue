580191e721dbc3f404531b602f23cde7e252289c
Douglas Calora <douglascalora123@gmail.com>
2020-03-09 11:45:44 -0300

    added prop default to row

--- src/components/TableGenerator.vue
+++ src/components/TableGenerator.vue
@@ -2,7 +2,7 @@
   <q-table class="bg-transparent" v-bind="attributes">
     <template v-for="(slot, key) in $scopedSlots" v-slot:[key]="context">
       <q-td :key="key" :props="context">
-        <slot :name="key" v-bind="context" :raw="raw" />
+        <slot :name="key" v-bind="context"/>
       </q-td>
     </template>
   </q-table>
@@ -64,9 +64,9 @@ export default {
     resultsByFields () {
       const results = extend(true, [], this.results)
 
-      return results.map(result => {
+      return results.map((result, index) => {
         for (const key in result) {
-          this.raw[key] = result[key]
+          result.default = this.results[index]
           result[key] = humanize(this.fields[key], result[key])
         }
 
