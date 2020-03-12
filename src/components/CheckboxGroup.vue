458968d3ce320948ba23cef4197d7a2c1f236943
Douglas Calora <douglascalora123@gmail.com>
2020-03-12 17:15:03 -0300

    implementing checkall

--- src/components/CheckboxGroup.vue
+++ src/components/CheckboxGroup.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
-    <div v-for="(option, index) in options" :key="index">
+  <div class="full-width">
+    <q-checkbox v-model="checkAll" label="Selecionar todas" />
+    <div v-for="(option, index) in options" :key="index" class="full-width">
       <q-checkbox :class="hasChildren(option) ? titleClass : []" :label="option.label" :value="hasChildren(option) ? group[index] : option.value" @input="updateCheckbox($event, option, index)" />
 
-      <q-option-group v-if="hasChildren(option)" class="q-ml-sm" inline :options="option.children" type="checkbox" :value="value" @input="updateChildren($event, option, index)" />
+      <q-option-group v-if="hasChildren(option)" class="q-ml-sm row col full-width" inline :options="option.children" type="checkbox" :value="value" @input="updateChildren($event, option, index)" />
     </div>
   </div>
 </template>
 
 <script>
+import { every } from 'lodash'
 
 export default {
   props: {
@@ -25,13 +27,24 @@ export default {
 
   data () {
     return {
-      group: {}
+      group: {},
+      checkAll: false
     }
   },
 
   computed: {
     titleClass () {
       return ['text-weight-bold']
+    },
+
+    groupList () {
+      const groupList = []
+
+      for (const key in this.group) {
+        groupList.push(this.group[key])
+      }
+
+      return groupList
     }
   },
 
@@ -40,8 +53,29 @@ export default {
   },
 
   watch: {
-    options () {
+    options (value) {
       this.handleParent()
+    },
+
+    checkAll (value) {
+      const updatedValue = []
+
+      if (value) {
+        this.options.forEach(option => option.children.forEach(child => updatedValue.push(child.value)))
+      }
+
+      for (const key in this.group) {
+        this.$set(this.group, key, value)
+      }
+
+      this.$emit('input', updatedValue)
+    },
+
+    value () {
+      // if (this.groupList.every(value => value)) {
+      //   this.checkAll = true
+      // }
+      this.checkAll = this.groupList.every(value => value)
     }
   },
 
