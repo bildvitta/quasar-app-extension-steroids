37172b73012e947d619fe8e23063a3e45839cd30
Douglas Calora <douglascalora123@gmail.com>
2020-03-26 17:40:10 -0300

    implementing imask

--- src/components/Mask.vue
+++ src/components/Mask.vue
@@ -1,12 +1,13 @@
 <template>
   <q-field v-model="text" class="full-width" v-bind="$attrs" stack-label contenteditable="true">
     <input ref="field" :value="text" @input="handleInput" class="full-width" :style="{ all: 'initial'}" type="text" placeholder="ahhh">
-    <!-- <q-input />; -->
   </q-field>
 </template>
 
 <script>
 import IMask from 'imask'
+import { cloneDeep } from 'lodash'
+
 let mask = null
 
 export default {
@@ -26,33 +27,35 @@ export default {
 
   mounted () {
     this.field = this.$refs.field
+    console.log(this.field instanceof Element)
 
     mask = IMask(this.field, {
       mask: [
-        {
-          mask: '+{7}(000)000-00-00'
-        },
-        {
-          mask: '(00) 00000-0000'
-        }
+        { mask: "(00) 0000-0000" },
+        { mask: "(00) 00000-0000" }
       ]
     })
   },
 
   watch: {
     text (value) {
-      console.log(mask, 'text')
+      // console.log(mask, 'text')
       // this.$emit('input', value)
+      // console.log(value)
+      mask.value = value || ''
+      this.$emit('input', mask.unmaskedValue)
     }
   },
 
   methods: {
     handleInput (value) {
       this.text = value ? value.target.value : ''
-      const {unmaskedValue} = mask
-      console.log(unmaskedValue, '>> valor sem mascara')
+      mask.value = this.text
+      // const {unmaskedValue} = mask
+      // console.log(extendMask.unmaskedValue, '>> valor sem mascara')
       this.$emit('input', mask.unmaskedValue)
-    }
+      // this.$emit('input', this.text)
+    },
   }
 }
 </script>
