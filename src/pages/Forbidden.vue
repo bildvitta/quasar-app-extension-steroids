a7f1f1950286f302aee4e88a88f435c054e07aa0
Douglas Calora <douglascalora123@gmail.com>
2020-09-04 15:47:36 -0300

    changes for code review

--- src/pages/Forbidden.vue
+++ src/pages/Forbidden.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="fixed-center text-center">
-    <img alt="" :src="srcPath">
+    <img alt="" src="../assets/sad.svg">
 
     <p class="q-my-lg text-faded">Ops, você não tem permissão para acessar esta página <strong>(403).</strong></p>
     <q-btn color="primary" flat @click="$router.push('/')">Voltar</q-btn>
   </div>
 </template>
-
-<script>
-export default {
-  computed: {
-    srcPath () {
-      return require('../assets/sad.svg')
-    }
-  }
-}
-</script>
--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,18 +1,8 @@
 <template>
   <div class="fixed-center text-center">
-    <img alt="" :src="srcPath">
+    <img alt="" src="../assets/sad.svg">
 
     <p class="q-my-lg text-faded">Ops, nada aqui... <strong>(404)</strong></p>
     <q-btn color="primary" flat @click="$router.push('/')">Voltar</q-btn>
   </div>
 </template>
-
-<script>
-export default {
-  computed: {
-    srcPath () {
-      return require('../assets/sad.svg')
-    }
-  }
-}
-</script>
